991dbb3843720294e0f8a2c2664a6ca2845cf57a
mla4783 <mla4783@rit.edu>
2020-11-23 12:53:38 -0500

    Skybox look cool

--- C20_EntityManager/AppClass.cpp
+++ C20_EntityManager/AppClass.cpp
@@ -123,7 +123,7 @@ void Application::Display(void)
 	ClearScreen();
 	
 	// draw a skybox
-	m_pMeshMngr->AddSkyboxToRenderList();
+	m_pMeshMngr->AddSkyboxToRenderList("Skybox.png");
 	
 	//render list call
 	m_uRenderCallCount = m_pMeshMngr->Render();
